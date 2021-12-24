f19e9b8938bbb57cf05d6a957e9dab33251e87b5
mihailovily <mihailovily@riseup.net>
2021-12-24 21:06:53 +0300

    бесконечные комментарии в говнокод

--- main.py
+++ main.py
@@ -4,55 +4,67 @@ from level_field import LevelField
 import loading_screen
 from menu import Menu
 import cube
-# import antipirate
+import antipirate
 import os
 
-# antipirate.generate_license()
+# потом убрать надо
+antipirate.generate_license()
+
+# определение размеров экрана
 root = Tk()
 height = root.winfo_screenheight()
 width = root.winfo_screenwidth()
-
+# инициализация, антипиратство
 pygame.init()
 size = width, height
 screen = pygame.display.set_mode(size)
-# running = antipirate.check_license()
+running = antipirate.check_license()
 running = True
 
+#менюшка
 menu = Menu(screen, width, height)
 
+# preloader
 loading = loading_screen.Loading(width, height, screen)
 
+# поле
 field = LevelField(width, height, screen)
 
+# время
 clock = pygame.time.Clock()
+
 while running:
     events = pygame.event.get()
     screen.fill((0, 0, 0))
     for event in events:
+        # выход по закрытию программы
         if event.type == pygame.QUIT:
             running = False
     if bool(events):
+        # у Влада спросить надо
         events = [events[0]]
     if loading.loading:
         loading.show()
     else:
         menu.show()
         for event in events:
+            # выход по нажатию на крестик
             if event.type == pygame.MOUSEBUTTONDOWN and (event.pos[0] - 60) ** 2 + (event.pos[0] - 60) ** 2 < 50 ** 2:
-                menu.showExit()
-        # field.show()
-        # for event in events:
-        #     if event.type == pygame.MOUSEBUTTONDOWN:
-        #         field.cube_jump()
-        #         field.show()
-        #     elif event.type == pygame.KEYDOWN:
-        #         if event.key == pygame.K_SPACE:
-        #             field.cube_jump()
-        #             field.show()
-    # pygame.draw.rect(screen, (0, 0, 0), (400, height - height // 4 - 70, 70, 70))
-    # pygame.draw.rect(screen, (0, 0, 0), (470, height - height // 4 - 70, 70, 70))
-    # pygame.draw.rect(screen, (0, 0, 0), (540, height - height // 4 - 70, 70, 70))
-    pygame.display.flip()
-# if os.path.exists("screenshot.jpg"):
-#     os.remove("screenshot.jpg")
+                running = False
+        # игровой процесс, пока что заморожен
+        if True == False:
+            field.show()
+            for event in events:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    field.cube_jump()
+                    field.show()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        field.cube_jump()
+                        field.show()
+        pygame.display.flip()
+# подчистка следов
+if os.path.exists("screenshot.jpg"):
+    os.remove("screenshot.jpg")
+# выход
 pygame.quit()
